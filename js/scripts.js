fb4dbe594c923df50ced041e7ca19ce342547671
narvils <narvils@gmail.com>
2025-01-30 10:50:42 +0200

    -

--- js/scripts.js
+++ js/scripts.js
@@ -302,3 +302,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
     animatedElements.forEach(element => observer.observe(element));
 });
+
+function watchForHover() {
+    let lastTouchTime = 0
+  
+    function enableHover() {
+      if (new Date() - lastTouchTime < 500) return
+      document.body.classList.add('hasHover')
+    }
+  
+    function disableHover() {
+      document.body.classList.remove('hasHover')
+    }
+  
+    function updateLastTouchTime() {
+      lastTouchTime = new Date()
+    }
+  
+    document.addEventListener('touchstart', updateLastTouchTime, true)
+    document.addEventListener('touchstart', disableHover, true)
+    document.addEventListener('mousemove', enableHover, true)
+  
+    enableHover()
+  }
+  
+  watchForHover()
